4286a0c226c3e78b08d327816df9b89c47d87d1a
Nikita Metelev <Kyle.B@mail.ru>
2026-06-04 22:36:29 +0500

    fix version

--- src/components/page/ts/Footer.tsx
+++ src/components/page/ts/Footer.tsx
@@ -28,10 +28,10 @@ const Footer: FC = () => {
 
       </Col>
 
-      <Col md={5} style={{textAlign: 'center', marginTop: '10px', color: 'white', fontSize: '12px'}}>V 3.5 *stable version</Col>
+      <Col md={5} style={{textAlign: 'center', marginTop: '10px', color: 'white', fontSize: '12px'}}>V 3.09</Col>
 
 
-      <Col className='mt-3'><div className='footer-title'>Производство РА "Центр творчества" &#169; Все права защищины</div></Col>
+      <Col className='mt-3'><div className='footer-title'>Производство UTV</div></Col>
       <Col className='mt-2 mb-4'><div className='footer-subtitle'>Техническая поддержака: 8-989-951-9063</div></Col>
 
     </Row>
